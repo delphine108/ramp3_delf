9d7d97d6c7cb9c4f1e590054cc44e065c1433f28
unknown <delphinel@gmail.com>
2015-04-10 14:46:38 +0200

    feature selection

--- classifier.py
+++ classifier.py
@@ -2,13 +2,16 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
+from sklearn.feature_selection import SelectPercentile
  
 class Classifier(BaseEstimator):
     def __init__(self):
         self.clf = Pipeline([
             ('imputer', Imputer(strategy='most_frequent')),
+            ('select', SelectPercentile(percentile=90)),
             ('rf', AdaBoostClassifier(
-                base_estimator=RandomForestClassifier(max_depth=5, n_estimators=10),
+                base_estimator=RandomForestClassifier(
+                  max_depth=5, n_estimators=30),
                 n_estimators=10)
             )
         ])
